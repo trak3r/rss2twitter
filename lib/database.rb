11cd978c472f855e7f23789c583dfc7e3690ebe8
Teflon Ted <github@rudiment.net>
2009-02-21 11:21:00 -0500

    major overhaul.  no more yaml configuration file.  all necessary info is passed on command line.  separate database created for each screen name (stored in run directory, so be careful).  converted to jeweler for gem management.

--- lib/database.rb
+++ lib/database.rb
@@ -1,48 +1,16 @@
 require 'active_record'
-require 'shorturl'
 
 ActiveRecord::Base.logger = Logger.new(STDERR)
 ActiveRecord::Base.colorize_logging = false
 
-ActiveRecord::Base.establish_connection(
-    :adapter => "sqlite3",
-    :dbfile  => 'rss2twitter.db'
-)
+class TweetsDatabase
 
-class Item < ActiveRecord::Base
-
-  def to_s
-    "#{self.optimized_title[0..(tweet_limit-self.short_url.length)]} #{self.short_url}"
-  end
-  
-  protected 
-  
-  def tweet_limit
-    138 # leave one off for fudging
-  end
-
-  def short_url
-    @cached_short_url ||= ShortURL.shorten(self.link, :fyad)
-  end
-  
-  def optimized_title
-    # for Trac feeds (which is why I wrote this) strip off some
-    # extraneous verbiage to save every last precious character
-    tidbits = self.title.scan( /^Changeset \[(.*?)\]\: (.*)/ ).flatten
-    if 2 == tidbits.length
-      return tidbits[1] # sprintf( "%s %s", *tidbits ) 
-    else # not a Trac changeset or we failed to parse it
-      return self.title
-    end
+  def initialize(screen_name)
+    ActiveRecord::Base.establish_connection(
+      :adapter => "sqlite3",
+      :dbfile  => "rss2twitter_#{screen_name}.db"
+    )
   end
 
 end
 
-unless Item.table_exists?
-  ActiveRecord::Schema.define do
-    create_table :items do |table|
-        table.column :title, :string
-        table.column :link, :string
-    end
-  end
-end
